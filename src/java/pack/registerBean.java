c6d30dc3a587605a0579ac99220e07ebdb1611bf
SongLei <740866392@qq.com>
2018-06-07 18:56:50 +0800

    DAO模式代码优化

--- src/java/pack/registerBean.java
+++ src/java/pack/registerBean.java
@@ -5,6 +5,8 @@
  */
 package pack;
 
+import dao.UserDao;
+import dao.UserDaoImpl;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -30,9 +32,7 @@ import pack.UserProfile;
  * @author sl
  */
 public class registerBean extends HttpServlet {
-            Connection conn;
-            PrintWriter out;
-            ResultSet rs;
+
             String username;
             String password;
             String firstname;
@@ -46,13 +46,6 @@ public class registerBean extends HttpServlet {
             String email;
             int phone;
 
-    @Override
-    public void init() throws ServletException {
-        super.init(); //To change body of generated methods, choose Tools | Templates.
-        conn=null;
-        out=null;
-        rs=null;
-    }
         
      @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -60,17 +53,7 @@ public class registerBean extends HttpServlet {
         super.doGet(request, response);
         }
     
-    @Override
-    public void destroy() {
-        super.destroy(); //To change body of generated methods, choose Tools | Templates.
-        try{
-        conn.close();
-        out.close();
-        rs.close();
-        }catch(SQLException se){
-            out.println(se.toString());
-        }
-    }
+
 
     /**
      *
@@ -83,8 +66,9 @@ public class registerBean extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //405错误这里 不要加super
         response.setContentType("text/html;charset=UTF-8");
-      out=response.getWriter();
-//     int userId = Integer.parseInt(request.getParameter("username"));
+
+
+//获取register的JSP页面提交的数据
        username= request.getParameter("username");
        password=request.getParameter("password");
          firstname=request.getParameter("firstname");
@@ -102,63 +86,31 @@ public class registerBean extends HttpServlet {
             e.printStackTrace();
         }
           
-        try{
-        Class.forName("com.mysql.jdbc.Driver").newInstance(); 
-        conn=DriverManager.getConnection("jdbc:mysql://localhost/mydb","root","SL886886");
-        UserProfile vo=new UserProfile();
-        vo.setUsername(username);
-        vo.setPassword(password);
-        vo.setFirstname(firstname);
-        vo.setMiddlename(middlename);
-        vo.setLastname(lastname);
-        vo.setAddress1(address1);
-        vo.setAddress2(address2);
-        vo.setCity(city);
-        vo.setState(state);
-        vo.setPincode(pincode);
-        vo.setEmail(email);
-        vo.setPhone(phone);
-        Boolean isSuccess=doCreate(vo);
-      
-//        if(userId==all.get(i).getUserId()&&password.equals(all.get(i).getUserPassword()))
-//        out.println(all.get(i).getUserPassword());
-         if(isSuccess==true)
-        {
-            
-//               HttpSession session=request.getSession();
-//             String usernameString=(String)session.getAttribute("username");
-             response.sendRedirect("registersuccess.html");
-        }else{
-            out.println("注册失败");
-          
-        } 
-        }catch(Exception e){
-          e.printStackTrace();
-        }
+           UserProfile user=new UserProfile();
+           //将数据与VO类进行交互
+           user.setUsername(username);
+        user.setPassword(password);
+        user.setFirstname(firstname);
+        user.setMiddlename(middlename);
+        user.setLastname(lastname);
+        user.setAddress1(address1);
+        user.setAddress2(address2);
+        user.setCity(city);
+        user.setState(state);
+        user.setPincode(pincode);
+        user.setEmail(email);
+        user.setPhone(phone);
+           //与数据层进行交互
+           UserDao dao=new UserDaoImpl();
+         boolean flag=dao.register(user);
+         if(flag){
+        response.sendRedirect("registersuccess.html");
+     }else{
+         response.sendRedirect("register.jsp");
     }
 
-   //查询全部数据 数据查询
-        public boolean doCreate(UserProfile vo) throws Exception {//vo只是作为了一个媒介 要想理解此次操作 还是看pstmt.setXXX是用来增加数据的 对应数据库的操作
-          PreparedStatement pstmt=null;
-          String query=null;
-            String sql = "INSERT INTO user_profile(username, password, firstname, middlename, lastname, address1, address2, city, state, pincode, email, phone) "  
-	            + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";  
-        pstmt=conn.prepareStatement(sql);
-        pstmt.setString(1, vo.getUsername());
-        pstmt.setString(2, vo.getPassword());
-        pstmt.setString(3, vo.getFirstname());
-        pstmt.setString(4, vo.getMiddlename());
-        pstmt.setString(5, vo.getLastname());
-        pstmt.setString(6, vo.getAddress1());
-        pstmt.setString(7, vo.getAddress2());
-        pstmt.setString(8, vo.getCity());
-        pstmt.setString(9, vo.getState());
-        pstmt.setString(10, vo.getPincode());
-        pstmt.setString(11, vo.getEmail());
-        pstmt.setInt(12, vo.getPhone());
-        
-        return pstmt.executeUpdate()>0;//如果为真 就是执行了操作 返回true1 否则返回false0
     }
+
     
     }
 
